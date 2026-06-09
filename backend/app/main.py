50e1ba9d654baabbd7b5b125b2c58be6b88620ae
D Shiva Reddy <devarashivareddy77@gmail.com>
2026-06-09 14:38:25 +0530

    process uploaded image with opencv

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,32 @@
 from fastapi import FastAPI, UploadFile, File
+import shutil
+import cv2
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
     return {
-        "message":"Deepfake Detector Running"
+        "message": "Deepfake Detector Running"
     }
+
 
 @app.post("/upload")
 async def upload_image(file: UploadFile = File(...)):
+
+    file_path = f"uploads/{file.filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    image = cv2.imread(file_path)
+
+    height, width = image.shape[:2]
+
     return {
         "filename": file.filename,
-        "status": "uploaded successfully"
+        "height": height,
+        "width": width,
+        "status": "processed"
     }
