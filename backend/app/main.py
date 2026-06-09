a22ce75f062ae8f487fd1223c0b1321b8cc42b15
D Shiva Reddy <devarashivareddy77@gmail.com>
2026-06-09 14:22:15 +0530

    add upload api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {
-        "message": "Deepfake Detector Running"
+        "message":"Deepfake Detector Running"
+    }
+
+@app.post("/upload")
+async def upload_image(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "status": "uploaded successfully"
     }
