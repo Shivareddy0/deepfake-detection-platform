5f80082f8b4e7be5ba69caf6f5e8a7d6084129f2
D Shiva Reddy <devarashivareddy77@gmail.com>
2026-06-10 00:29:39 +0530

    complete frontend backend integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import shutil
 from app.detector import detect_fake
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
