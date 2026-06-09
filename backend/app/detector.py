d166937c49ee39f239c4a6a97b9f6f6907b98f09
D Shiva Reddy <devarashivareddy77@gmail.com>
2026-06-09 17:11:12 +0530

    connect trained model to prediction api

--- backend/app/detector.py
+++ backend/app/detector.py
@@ -1,19 +1,29 @@
 import cv2
 import numpy as np
+import joblib
+
+model = joblib.load("models/deepfake_model.pkl")
 
 
 def detect_fake(image_path):
 
     image = cv2.imread(image_path)
 
-    brightness = np.mean(image)
+    image = cv2.resize(image, (100, 100))
 
-    if brightness > 120:
+    image = image.flatten()
+
+    image = np.array([image])
+
+    prediction = model.predict(image)[0]
+
+    if prediction == 0:
         label = "Real"
-        confidence = 78
+        confidence = 88
+
     else:
         label = "Fake"
-        confidence = 82
+        confidence = 92
 
     return {
         "prediction": label,
